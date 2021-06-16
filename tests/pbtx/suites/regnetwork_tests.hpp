362f4db29e23c353a6414f7d8a41a4806688469d
Artem <artem.v.maluga@gmail.com>
2021-06-16 15:19:54 +0300

    Fix regnetwork

--- tests/pbtx/suites/regnetwork_tests.hpp
+++ tests/pbtx/suites/regnetwork_tests.hpp
@@ -6,7 +6,7 @@ BOOST_FIXTURE_TEST_CASE(asserts_regnetwork_test, pbtx_tester)
 try
 {
     BOOST_REQUIRE_EQUAL(error("missing authority of bob"), 
-                        m_pbtx_api.regnetwork(N(alice), 1002, N(bob), {},  0xFFFFFFFF));
+                        m_pbtx_api.regnetwork(N(alice), 1002, N(bob), {},  0xFFFF0000));
 
     BOOST_REQUIRE_EQUAL(wasm_assert_msg("Unrecognized bits set in lower 16 bits of flags"),
                         m_pbtx_api.regnetwork(N(bob), 1002, N(bob), {},  0x00000000));
@@ -16,7 +16,7 @@ FC_LOG_AND_RETHROW()
 BOOST_FIXTURE_TEST_CASE(regnetwork_test, pbtx_tester)
 try
 {
-    BOOST_REQUIRE_EQUAL(success(), m_pbtx_api.regnetwork(N(bob), 1002, N(bob), {},  0x00000001));
+    BOOST_REQUIRE_EQUAL(success(), m_pbtx_api.regnetwork(N(bob), 1002, N(bob), {},  0xFFFF0001));
 
     auto network = m_pbtx_api.get_network(1002);
 
