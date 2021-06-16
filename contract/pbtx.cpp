c3f3df01976d21c4bac052edad8f0b5f2fd978ac
cc32d9 <cc32d9@gmail.com>
2021-06-16 20:56:41 +0200

    split Transaction into body and signature

--- contract/pbtx.cpp
+++ contract/pbtx.cpp
@@ -189,27 +189,32 @@ ACTION pbtx::exectrx(name worker, vector<uint8_t> trx_input)
   pbtx_Transaction trx;
   pb_istream_t trx_stream = pb_istream_from_buffer(trx_input.data(), trx_input.size());
   check(pb_decode(&trx_stream, pbtx_Transaction_fields, &trx), trx_stream.errmsg);
-  check(trx.transaction_content.size > 0, "Empty transaction");
+  check(trx.body.size > 0, "Empty transaction body");
 
+  pbtx_TransactionBody body;
+  pb_istream_t body_stream = pb_istream_from_buffer(trx.body.bytes, trx.body.size);
+  check(pb_decode(&body_stream, pbtx_TransactionBody_fields, &trx), body_stream.errmsg);
+  check(body.transaction_content.size > 0, "Empty transaction content");
+  
   networks _networks(_self, 0);
-  auto nwitr = _networks.find(trx.network_id);
+  auto nwitr = _networks.find(body.network_id);
   if( nwitr == _networks.end() ) {
-    check(false, "Unknown network_id: " + to_string(trx.network_id));
+    check(false, "Unknown network_id: " + to_string(body.network_id));
   }
 
-  actorperm _actorperm(_self, trx.network_id);
-  auto actpermitr = _actorperm.find(trx.actor);
+  actorperm _actorperm(_self, body.network_id);
+  auto actpermitr = _actorperm.find(body.actor);
   if( actpermitr == _actorperm.end() ) {
-    check(false, "Unknown actor: " + to_string(trx.actor));
+    check(false, "Unknown actor: " + to_string(body.actor));
   }
 
-  actorseq _actorseq(_self, trx.network_id);
-  auto actseqitr = _actorseq.find(trx.actor);
+  actorseq _actorseq(_self, body.network_id);
+  auto actseqitr = _actorseq.find(body.actor);
   check(actseqitr != _actorseq.end(), "Exception 2");
 
-  if( trx.seqnum != actseqitr->seqnum + 1 ) {
+  if( body.seqnum != actseqitr->seqnum + 1 ) {
     check(false, "Expected seqnum=" + to_string(actseqitr->seqnum + 1) +
-          ", received seqnum=" + to_string(trx.seqnum));
+          ", received seqnum=" + to_string(body.seqnum));
   }
 
   _actorseq.modify(*actseqitr, same_payer, [&]( auto& row ) {
@@ -217,18 +222,18 @@ ACTION pbtx::exectrx(name worker, vector<uint8_t> trx_input)
                                              row.last_modified = current_time_point();
                                            });
 
-  if( trx.signatures_count != trx.cosignors_count + 1 ) {
-    check(false, "Expected " + to_string(trx.cosignors_count + 1) + " signatures, but received " +
+  if( trx.signatures_count != body.cosignors_count + 1 ) {
+    check(false, "Expected " + to_string(body.cosignors_count + 1) + " signatures, but received " +
           to_string(trx.signatures_count));
   }
 
-  checksum256 digest = sha256((const char*)trx.transaction_content.bytes, trx.transaction_content.size);
+  checksum256 digest = sha256((const char*)body.transaction_content.bytes, body.transaction_content.size);
 
   validate_signature(digest, actpermitr->permission, trx.signatures[0]);
-  for( uint32_t i = 0; i < trx.cosignors_count; i++ ) {
-    actpermitr = _actorperm.find(trx.cosignors[i]);
+  for( uint32_t i = 0; i < body.cosignors_count; i++ ) {
+    actpermitr = _actorperm.find(body.cosignors[i]);
     if( actpermitr == _actorperm.end() ) {
-      check(false, "Unknown cosignor #" + to_string(i) + ": " + to_string(trx.cosignors[i]));
+      check(false, "Unknown cosignor #" + to_string(i) + ": " + to_string(body.cosignors[i]));
     }
     validate_signature(digest, actpermitr->permission, trx.signatures[i+1]);
   }
@@ -242,8 +247,8 @@ ACTION pbtx::exectrx(name worker, vector<uint8_t> trx_input)
   else {
     pbtxtransact_abi args =
       {
-       worker, trx.actor, trx.seqnum, {trx.cosignors, trx.cosignors + trx.cosignors_count},
-       {trx.transaction_content.bytes, trx.transaction_content.bytes + trx.transaction_content.size}
+       worker, body.actor, body.seqnum, {body.cosignors, body.cosignors + body.cosignors_count},
+       {body.transaction_content.bytes, body.transaction_content.bytes + body.transaction_content.size}
       };
 
     vector<permission_level> perms{permission_level{_self, name("active")},
